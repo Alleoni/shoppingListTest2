86167ff564766ba55808e17e245d3ca5533068d6
Guilherme Alleoni <guilherme.alleoni@yahoo.com.br>
2023-01-29 22:43:23 -0300

    Atualização nomes

--- src/shoppinglist/ShoppingList.java
+++ src/shoppinglist/ShoppingList.java
@@ -2,18 +2,13 @@ package shoppinglist;
 
 import java.util.ArrayList;
 import java.util.List;
-import shoppinglist.entities.Client;
 import shoppinglist.entities.Compra;
-import shoppinglist.method.CompraMethod;
+import shoppinglist.method.MetodoCompra;
 
 public class ShoppingList {
 
     public static void main(String[] args) {
         List<Compra> compras = new ArrayList<>();
-
-//        Client c1 = new Client("Marcus");
-//        Client c2 = new Client("Robson");
-//        Client c3 = new Client("Guilherme");
 
         Compra comp1 = new Compra(100, "Marcus");
         Compra comp2 = new Compra(150, "Marcus");
@@ -28,9 +23,9 @@ public class ShoppingList {
         compras.add(comp4);
         compras.add(comp5);
         compras.add(comp6);
-        
-        CompraMethod.returnShoppingList(compras);
-        
+
+        MetodoCompra.metodoCompra(compras);
+
     }
 
 }
--- src/shoppinglist/method/MetodoCompra.java
+++ src/shoppinglist/method/MetodoCompra.java
@@ -4,22 +4,21 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import shoppinglist.entities.Client;
 import shoppinglist.entities.Compra;
 
-public class CompraMethod {
+public class MetodoCompra {
 
-    public static void returnShoppingList(List<Compra> compras) {
+    public static void metodoCompra(List<Compra> compras) {
         Map<String, Integer> mapCompraPorCliente = new HashMap<>();
         Map<String, Integer> mapValorPorCliente = new HashMap<>();
         for (Compra compra : compras) {
             Integer comprasDoCliente = mapCompraPorCliente.getOrDefault(compra.getClient(), 0);
-
             comprasDoCliente++;
+
             mapCompraPorCliente.put(compra.getClient(), comprasDoCliente);
 
             mapValorPorCliente.put(compra.getClient(), compra.getValor());
-
+            
             System.out.println("Cliente: " + compra.getClient() + " Valor: R$" + compra.getValor());
         }
 
@@ -30,11 +29,7 @@ public class CompraMethod {
         System.out.println();
         System.out.println("O Cliente que mais comprou em valores foi o " + maiorValor);
         System.out.println("O Cliente que mais fez compras foi o " + qtdCompra);
-        
+
     }
 
 }
-//- Crie um método que receba uma lista de compras, com dados do cliente e valor 
-//- Imprima o valor de compra por cliente.
-//- Imprima o cliente que mais comprou em valores.
-//- Imprima o cliente que teve o maior numero de compras.
